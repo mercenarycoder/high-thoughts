cc31fcf204960a1dbaac161f9a74da520c4ec10e
Manjeet Singh <67825575+mercenarycoder@users.noreply.github.com>
2024-07-04 18:29:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    getNames();
+  }, []);
+
+  const getNames = async () => {
+    let response = await axios.get("/names");
+    console.log(response);
+    // response = await response.json();
+    setUsername(response.data);
+  };
+  return (
+    <div className="App">
+      <h2>Hey learning the CI-CD with github actions.</h2>
+      <h1>{username}</h1>
+    </div>
+  );
+}
+
+export default App;
